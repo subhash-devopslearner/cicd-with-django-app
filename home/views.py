d69e690a3b47438196457caeade43f2ea6518f22
Subhash Bhalerao <subhash-devopslearner@gmail.com>
2026-03-10 16:03:03 +0530

    Update Docker Compose files for production and staging environments; remove test configuration and adjust .gitignore for environment files

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Welcome to CICD testing with Django app 🚀')
+    environment = os.getenv('ENVIRONMENT', 'development')
+    return HttpResponse(f'Welcome to CICD testing with Django app 🚀 (Environment: {environment})')
